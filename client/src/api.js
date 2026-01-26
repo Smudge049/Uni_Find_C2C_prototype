3730532c236d56555911ff6acbf1d1de01c69fea
Ankit Sapkota <sapkotankit49@gmail.com>
2026-01-26 18:52:15 +0545

    UI enhancement

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api', // Proxy will divert this to http://localhost:3000
+    baseURL: import.meta.env.VITE_API_URL || '/api',
     headers: {
         'Content-Type': 'application/json',
     },
